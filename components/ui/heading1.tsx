262c010103905c86a19f137a1b84004ecf9c9375
gileanu <gileanu@gmail.com>
2024-02-29 18:52:08 +0200

    [fix] better H1 component

--- components/ui/heading1.tsx
+++ components/ui/heading1.tsx
@@ -7,7 +7,7 @@ export const Heading1: React.FC<HeadingProps> = ({ title, description }) => {
   return (
     <div>
       <h1 className="text-2xl lg:text-4xl font-bold tracking-tight">{title}</h1>
-      <p className="text-sm text-muted-foreground">{description}</p>
+      <p className="text-sm text-muted-foreground pt-2">{description}</p>
     </div>
   );
 };

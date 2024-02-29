1221a95c456b37f38b8d055e52b9680c304d6409
gileanu <gileanu@gmail.com>
2024-02-29 18:51:37 +0200

    [fix] updated footer info

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,14 +5,14 @@ const currnetYear = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <footer className="bg-white border-t h-16">
+    <footer className="border-t h-16">
       <Container>
         <div className="mx-auto py-5">
-          <p className="test-xs text-black">
-            &copy; {currnetYear} [name], [rights] - [terms] [conditions].
-          </p>
+          <p className="test-xs ">&copy; {currnetYear} viShoots</p>
           <p className="text-xs text-gray-500">
-            <a href="https://github.com/gileanu">RR - github.com/gileanu</a>
+            <a href="https://github.com/gileanu" target="_blank">
+              RR - github.com/gileanu
+            </a>
           </p>
         </div>
       </Container>

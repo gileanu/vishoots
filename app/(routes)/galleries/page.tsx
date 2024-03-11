26eb6f4944912c5328a3f47c6f4bb90c125a5990
gileanu <gileanu@gmail.com>
2024-03-11 22:21:07 +0200

    [fix] fixed slugs

--- app/(routes)/galleries/page.tsx
+++ app/(routes)/galleries/page.tsx
@@ -1,15 +1,20 @@
+import getBillboard from "@/actions/get-billboard";
 import getBillboards from "@/actions/get-billboards";
 import getCategories from "@/actions/get-categories";
 import Container from "@/components/page-container";
+import Billboard from "@/components/ui/billboard";
 
 const GalleryPageCategories = async () => {
-  const billboards = await getBillboards();
+  const billboard = await getBillboard("35fb312e-89dd-44d3-9cf5-ae0642a086b8");
   const categories = await getCategories();
   console.log(categories);
   return (
-    <Container>
-      <div>Gallery Page by categories</div>
-    </Container>
+    <>
+      <Billboard data={billboard} />
+      <Container>
+        <div>Gallery Page by categories</div>
+      </Container>
+    </>
   );
 };
 

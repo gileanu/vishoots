7e97c80fcea774da1b66f7e21d86e94144b6f621
gileanu <gileanu@gmail.com>
2024-10-06 15:46:26 +0300

    [misc] removed const revalidate on homepage

--- components/featured-galleries.tsx
+++ components/featured-galleries.tsx
@@ -3,8 +3,6 @@ import getGalleries from "@/actions/get-galleries";
 import NoResults from "./no-results";
 import GalleryCard from "./gallery-card";
 
-export const revalidate = 0;
-
 const FeaturedGalleries = async () => {
   const galleries = await getGalleries({ isFeatured: true });
   return (
